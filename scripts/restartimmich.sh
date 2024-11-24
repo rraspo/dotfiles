29ccf94f3874b2972d5620408da3afe708520ac4
Juan A. Porras <juanabrahamporras@gmail.com>
2024-11-24 03:08:24 -0600

    Update restartimmich.sh

--- scripts/restartimmich.sh
+++ scripts/restartimmich.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 # Script to restart the Immich Docker Compose setup and return to the original directory
 
-prevdir=$(pwd)  # Save the current directory
-cd ~/src/immich-app || exit 1  # Change to the Immich directory or exit
-sudo docker compose down  # Stop the containers
-sudo docker compose up -d  # Start the containers
-cd "$prevdir" || exit 1  # Return to the previous directory
+prevdir=$(pwd)
+cd ~/src/immich-app || exit 1
+sudo docker compose down
+sudo docker compose up -d
+cd "$prevdir" || exit 1
