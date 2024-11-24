074cab9037fa47d0a89194e7bce5111ae6db2b8b
Juan A. Porras <juanabrahamporras@gmail.com>
2024-11-24 03:07:49 -0600

    Update prompt.zsh

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,7 +1,7 @@
 # Prefered prompt with git plugin
 
-ZSHRC_USER_HOST="%F{cyan}%n@%m:%f"
-ZSHRC_CURRENT_DIR="%F{cyan}%~%f"
-ZSHRC_CONDITIONAL="%(?.%F{green}.%F{red})➜ %f"
+ZSHRC_USER_HOST="%F{cyan}%n@%m:%f" # user@host
+ZSHRC_CURRENT_DIR="%F{cyan}%~%f" # current dir
+ZSHRC_CONDITIONAL="%(?.%F{green}.%F{red})➜ %f" # colored arrow
 PROMPT="${ZSHRC_USER_HOST}${ZSHRC_CURRENT_DIR} ${ZSHRC_CONDITIONAL}"
 PROMPT+=' $(git_prompt_info)'
