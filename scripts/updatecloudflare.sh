f822d0c3e9eb5db3dea210edfcb082ce3059ee6d
Abraham <porras@Motherboard.>
2024-11-30 13:50:41 -0600

    Fix subdomain dns update and json body gen function

--- scripts/updatecloudflare.sh
+++ scripts/updatecloudflare.sh
@@ -20,36 +20,39 @@ get_public_ip() {
 
 get_dns_record() {
   DOMAIN=$1
-  curl -s -X GET "$API_BASE/zones/$ZONE_ID/dns_records?name=auth.$DOMAIN" \
-    --header "X-Auth-Email: $EMAIL" \
-    --header "X-Auth-Key: $API_KEY" \
-    -H "Content-Type:application/json"
+  SUBDOMAIN=$2
+  if [[ -z "$2" ]] then
+    curl -s -X GET "$API_BASE/zones/$ZONE_ID/dns_records?name=$DOMAIN" \
+      --header "X-Auth-Email: $EMAIL" \
+      --header "X-Auth-Key: $API_KEY" \
+      -H "Content-Type:application/json"
+  else
+    curl -s -X GET "$API_BASE/zones/$ZONE_ID/dns_records?name=$SUBDOMAIN.$DOMAIN" \
+      --header "X-Auth-Email: $EMAIL" \
+      --header "X-Auth-Key: $API_KEY" \
+      -H "Content-Type:application/json"
+  fi
 }
 
 update_dns_record() {
   CURRENT_IP=$1
   RECORD_ID=$2
-  if [[ -z "$3" ]] then
-    SUBDOMAIN=""
-  else
-    SUBDOMAIN="$S3."
-  fi
   echo "DNS Record: $(get_dns_record)"
   echo "$API_BASE/zones/$ZONE_ID/dns_records/$RECORD_ID"
   echo "Zone: $ZONE_ID"
   echo "Record: $RECORD_ID"
-  echo "$(generate_post_data)"
+  echo "$(generate_post_data "$SUBDOMAIN")"
   curl --request PATCH \
     --url "$API_BASE/zones/$ZONE_ID/dns_records/$RECORD_ID" \
     --header "X-Auth-Email: $EMAIL" \
     --header "X-Auth-Key: $API_KEY" \
     --header "Content-Type: application/json" \
-    --data "$(generate_post_data)"
+    --data "$(generate_post_data "$SUBDOMAIN")"
 }
 
 generate_post_data() {
   local SUBDOMAIN=$1
-  if [[ -z "$SUBDOMAIN" ]]; then
+  if [[ -z "$1" ]]; then
     cat <<EOF
 {
     "comment": "Domain verification record",
@@ -86,19 +89,23 @@ if [ -z "$PUBLIC_IP" ]; then
   echo "Error: Unable to fetch public IP."
   exit 1
 fi
-echo $RECORD_ID
 echo "Getting DNS record..."
 RESPONSE_ID=$(get_dns_record "$DOMAIN" | jq -r ".result[0].id")
 echo $RESPONSE_ID
-get_dns_record | jq
+get_dns_record "$DOMAIN" | jq
+printf "\n"
 update_dns_record "$PUBLIC_IP" "$RESPONSE_ID"
 
 if [[ -n "$SUBDOMAINS" ]]; then
   echo "Subdomains are set: $SUBDOMAINS"
   IFS=' ' read -r -a SUBDOMAINS <<<"$SUBDOMAINS"
   for SUBDOMAIN in "${SUBDOMAINS[@]}"; do
-    echo "Processing subdomain: $SUBDOMAIN"
+    printf "\nProcessing subdomain: $SUBDOMAIN"
+    printf "\nGetting DNS record for $SUBDOMAIN...\n"
+    RESPONSE_ID=$(get_dns_record "$DOMAIN" "$SUBDOMAIN" | jq -r ".result[0].id")
+    echo $RESPONSE_ID
+    get_dns_record "$DOMAIN" "$SUBDOMAIN" | jq
+    printf "\n"
     update_dns_record "$PUBLIC_IP" "$RESPONSE_ID" "$SUBDOMAIN" 
   done
-
 fi
