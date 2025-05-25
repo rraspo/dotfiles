a59e57d6d8f00dcb30845c713e96001e7dacdebf
Abraham <porras@duck.com>
2025-05-25 03:50:44 +0000

    Remove immich change

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,7 +3,6 @@
 # Navigation
 alias ll='ls -lia'
 alias rzsh='source ~/.zshrc'
-alias ri='prevdir=\$(pwd) && cd ~/src/immich-app && sudo docker compose down && sudo docker compose up -d && cd \$prevdir'
 
 # Alias to restart Immich server
 alias ri="~/.dotfiles/scripts/restartimmich.sh"
