72f1e061916dde3878b47b6e4ee8b6abbf2b15b1
Abraham PorraS <porras@duck.com>
2025-05-25 13:19:06 -0600

    Fix alias name

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,8 @@
 # Custom aliases and functions
 
 # Navigation
-alias ll='ls -lia'
-alias rzsh='source ~/.zshrc'
+alias ll='ls -liah'
+alias szsh='source ~/.zshrc'
 
 # Alias to restart Immich server
 alias ri="~/.dotfiles/scripts/restartimmich.sh"
